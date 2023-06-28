21164b14933c5a789fb73b2c63a8f8fc874a6194
dea <108593938+deaamanger@users.noreply.github.com>
2023-06-28 07:37:39 +0200

    logo anmition

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,9 +6,11 @@ export default function Header() {
   return (
     <div className="flex justify-between items-start px-6 pt-5  ">
       <img
-        src="https://cdn.dribbble.com/users/1161517/screenshots/7896076/media/24ae74ddb6c9eb7789ae3a189a6b30ae.gif"
+        //src="https://www.photofunky.net/output/image/9/7/5/e/975e28/photofunky.gif"
+        src="https://static.wixstatic.com/media/f6a5ec_1360d243252749d788adfd75184c9d1a~mv2.gif"
+        //src="https://cdn.dribbble.com/users/1161517/screenshots/7896076/media/24ae74ddb6c9eb7789ae3a189a6b30ae.gif"
         alt=""
-        className="h-12  mt-4 animate-pulse"
+        className="h-12 lg:h-16  mt-4 animate-pulse"
       />
       <h1 className=" text-gray-300 mt-6 animate-pulse hover:bg-gray-800 hover:text-amber-500 hover:rounded-xl hover:px-1 hover:py-1 cursor-pointer  ">
         <svg
